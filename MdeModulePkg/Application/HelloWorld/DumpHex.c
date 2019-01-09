bcd609b5fde1d4dfa9a6be7ec2ad7f8284033082
jiansonx <jiansongx.xu@intel.com>
2019-01-09 14:58:33 +0800

    add a char * format argument, to print in each begin of line

--- MdeModulePkg/Application/HelloWorld/DumpHex.c
+++ MdeModulePkg/Application/HelloWorld/DumpHex.c
@@ -21,6 +21,8 @@
 #include <Library/UefiRuntimeServicesTableLib.h>
 #include <Guid/GlobalVariable.h>
 #include <Library/MemoryAllocationLib.h>
+#include <stdarg.h>
+
 //
 // String token ID of help message text.
 // Shell supports to find help message in the resource section of an application image if
@@ -51,6 +53,47 @@ STATIC CONST CHAR8 Hex[] = {
 };
 
 
+
+
+
+#define HEX_DUMP(Indent, Offset, DataSize, UserData)                                           \
+{                                                                                             \
+  UINT8 *Data;                                                                                \
+                                                                                              \
+  CHAR8 Val[50];                                                                              \
+                                                                                              \
+  CHAR8 Str[20];                                                                              \
+                                                                                              \
+  UINT8 TempByte;                                                                             \
+  UINTN Size;                                                                                 \
+  UINTN Index;                                                                                \
+                                                                                              \
+  Data = (UINT8 *)(UserData);                                                                 \
+  while (DataSize != 0) {                                                                     \
+    Size = 16;                                                                                \
+    if (Size > DataSize) {                                                                    \
+      Size = DataSize;                                                                        \
+    }                                                                                         \
+                                                                                              \
+    for (Index = 0; Index < Size; Index += 1) {                                               \
+      TempByte            = Data[Index];                                                      \
+      Val[Index * 3 + 0]  = Hex[TempByte >> 4];                                               \
+      Val[Index * 3 + 1]  = Hex[TempByte & 0xF];                                              \
+      Val[Index * 3 + 2]  = (CHAR8) ((Index == 7) ? '-' : ' ');                               \
+      Str[Index]          = (CHAR8) ((TempByte < ' ' || TempByte > '~') ? '.' : TempByte);    \
+    }                                                                                         \
+                                                                                              \
+    Val[Index * 3]  = 0;                                                                      \
+    Str[Index]      = 0;                                                                      \
+    Print( L"%*a%08X: %-48a *%a*\r\n", Indent, "", Offset, Val, Str);                         \
+                                                                                              \
+    Data += Size;                                                                             \
+    Offset += Size;                                                                           \
+    DataSize -= Size;                                                                         \
+  }                                                                                           \
+} 
+
+
 /**
   Dump some hexadecimal data to the screen.
 
@@ -65,7 +108,9 @@ DumpHex (
   IN UINTN        Indent,
   IN UINTN        Offset,
   IN UINTN        DataSize,
-  IN VOID         *UserData
+  IN VOID         *UserData,
+  char            *format,
+  ...
   )
 {
   UINT8 *Data;
@@ -95,52 +140,21 @@ DumpHex (
 
     Val[Index * 3]  = 0;
     Str[Index]      = 0;
-    Print( L"%*a%08X: %-48a *%a*\r\n", Indent, "", Offset, Val, Str);
+    va_list args;
+    va_start(args, format);
+
+    if (*format == 'c') {
+       int c = va_arg(args, int);
+       Print(L"%c", (char)c);
+    }
+    Print( L"%*a%08X: %-48a *%a*\r\n", Indent, " ", Offset, Val, Str);
 
     Data += Size;
     Offset += Size;
     DataSize -= Size;
   }
 }
-
-
-
-#define HexDump(Indent, Offset, DataSize, UserData)   \
-{                                                     \
-  UINT8 *Data;      \
-                    \
-  CHAR8 Val[50];    \
-                    \
-  CHAR8 Str[20];    \
-                    \
-  UINT8 TempByte;    \
-  UINTN Size;      \
-  UINTN Index;     \
-                    \
-  Data = (UINT8 *)(UserData);     \
-  while (DataSize != 0) {   \
-    Size = 16;              \
-    if (Size > DataSize) {  \
-      Size = DataSize;    \
-    }                  \
-                       \
-    for (Index = 0; Index < Size; Index += 1) {   \
-      TempByte            = Data[Index];          \
-      Val[Index * 3 + 0]  = Hex[TempByte >> 4];    \
-      Val[Index * 3 + 1]  = Hex[TempByte & 0xF];    \
-      Val[Index * 3 + 2]  = (CHAR8) ((Index == 7) ? '-' : ' ');   \
-      Str[Index]          = (CHAR8) ((TempByte < ' ' || TempByte > '~') ? '.' : TempByte);   \
-    }            \
-                  \
-    Val[Index * 3]  = 0;         \
-    Str[Index]      = 0;        \
-    Print( L"%*a%08X: %-48a *%a*\r\n", Indent, "", Offset, Val, Str);  \
-      \
-    Data += Size;    \
-    Offset += Size;   \
-    DataSize -= Size;  \
-  }    \
-}     \
+                                                                                            \
 
 /**
   The user Entry Point for Application. The user code starts with this function
@@ -161,18 +175,22 @@ UefiMain (
   )
 {
   UINTN                   Length;
-  Length = 15;
+  Length = 32;
   UINT8                     *Buffer;
-  Buffer  = AllocatePool (4);
+  Buffer  = AllocatePool (18);
   Buffer[0] = 9;
   Buffer[1] = 1;
   Buffer[2] = 2;
   Buffer[3] = 5;
   Buffer[4] = 8;
-  UINTN aa  = 2;
-  UINTN bb = 0;
-  DumpHex (0, 0, Length, Buffer);
-  HexDump (bb, aa, Length, Buffer);
+  Buffer[18] = 9;
+  Buffer[19] = 10;
+
+  DumpHex (0, 0, Length, Buffer , "c", 'a', "c", 'b' );
+
+  // UINTN aa  = 3;
+  // UINTN bb = 0;
+  // HEX_DUMP (bb, aa, Length, Buffer);
 
   return EFI_SUCCESS;
 }
